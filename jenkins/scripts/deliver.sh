63aa4b7a1ba3f4537a70aa8894fdaa844365b205
Chris Anderson <topher.anderson42@gmail.com>
2023-10-07 01:55:51 -0400

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,11 @@ VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 ## set -x
-java -jar target/${NAME}-${VERSION}.jar
+
+echo "---------------"
+echo ${NAME}
+echo ${VERSION}
+echo ${NAME}-${VERSION}
+echo "---------------"
+
+## java -jar target/${NAME}-${VERSION}.jar
